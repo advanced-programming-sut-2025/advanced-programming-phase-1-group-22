b9c7f67a27277f4b04443f22e685c36a9242035f
Roham <roham.ghasemi03@gmail.com>
2025-04-20 22:06:13 +0330

    Added commands rejexes

--- src/main/java/view/gameMenu/TradeMenu.java
+++ src/main/java/view/gameMenu/TradeMenu.java
@@ -1,4 +1,39 @@
 package view.gameMenu;
 
-public class TradeMenu {
+import controller.gameMenu.StoreMenuController;
+import controller.gameMenu.TradeMenuController;
+import model.exception.InvalidInputException;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+
+public class TradeMenu extends GameMenu{
+    private final String trade = "^\\s*trade\\s+-u\\s+(?<username>.+)\\s+-t\\s+(?<type>\\boffer|request)\\s+-i\\s+" +
+            "(?<item>.+)\\s+-a\\s+(?<amount>\\d+)(\\s+-p\\s+(?<price>\\d+))?(\\s+-ti\\s+(?<targetItem>.+)\\s+-ta\\" +
+            "s+(?<targetAmount>\\d+))?\\s*$"; //TODO the possibility to offer target item first and price next
+    private final String tradeList = "^\\s*trade\\s+list\\s*$";
+    private final String tradeResponse = "^\\s*trade\\s+respond\\s+(?<respond>\\baccept|reject)\\s+-i\\s+(?<id>\\d+)\\s*$";
+    private final String tradeHistory = "^\\s*trade\\s+history\\s*$";
+    private final TradeMenuController controller = new TradeMenuController();
+
+    @Override
+    public void checkCommand(Scanner scanner) {
+        String input = scanner.nextLine();
+
+        Matcher matcher;
+
+        if (super.check(input)) {
+            return;
+        } else if (isMatched(input, tradeList) != null) {
+            controller.tradeList();
+        } else if (isMatched(input, tradeHistory) != null) {
+            controller.tradeHistory();
+        } else if ((matcher = isMatched(input, tradeResponse)) != null) {
+            controller.tradeResponse(matcher.group("respond"), matcher.group("id"));
+        } else if ((matcher = isMatched(input, trade)) != null) {
+            controller.trade(matcher);
+        } else {
+            throw new InvalidInputException("Invalid Command");
+        }
+    }
 }
