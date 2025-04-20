cad1ad551758780aa32a3d3e14d1b954de9672fd
Roham <roham.ghasemi03@gmail.com>
2025-04-21 02:26:41 +0330

    initializing TimeAndDate

--- src/main/java/model/TimeAndDate.java
+++ src/main/java/model/TimeAndDate.java
@@ -1,6 +1,5 @@
 package model;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,12 +15,39 @@ public class TimeAndDate {
     private Season season = Season.SPRING;
     private Integer day = 1;
 
+    public TimeAndDate() {}
+
     public TimeAndDate(int day, int hour) {
         this.hour = hour;
         this.day = day;
+        moveTimeForward();
     }
 
-    public void moveTimeForward(TimeAndDate timeAndDate) {
+//    public void moveTimeForward(TimeAndDate timeAndDate)) {
+//    }
 
+    public void moveTimeForward() {
+        minute += 15;
+        if (minute >= 60) {
+            hour ++;
+            minute = 0;
+        }
+        if (hour >= 21) {
+            day++;
+            hour = 9;
+            //TODO: reset Energy sets and randomizing the day
+        }
+        if (day >= 29) {
+            day = 1;
+            season = Season.values()[(season.ordinal() + 1) % 4];
+        }
+    }
+
+    public int compareTime(TimeAndDate timeAndDate) {
+        if (timeAndDate.getHour() > hour) return 1;
+        if (timeAndDate.getHour() < hour) return -1;
+        if (timeAndDate.getMinute() > minute) return 1;
+        if (timeAndDate.getMinute() < minute) return -1;
+        return 0;
     }
 }
