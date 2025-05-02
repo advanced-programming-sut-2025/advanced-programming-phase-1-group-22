101661692234a69f01c9755aa1da05209996c32c
Roham8406 <Roham8406@gmail.com>
2025-05-02 19:31:02 +0330

    debugging

--- src/main/java/command/GameInitCommands.java
+++ src/main/java/command/GameInitCommands.java
@@ -1,9 +1,9 @@
 package command;
 
 public class GameInitCommands extends CommandClass {
-    public static final GameInitCommands newGame = new GameInitCommands("^\\s*game\\s+new\\s+-u\\s+(?<players>.*)\\s*$");
+    public static final GameInitCommands newGame = new GameInitCommands("^\\s*game\\s+new\\s+-u(\\s+(.*))?\\s*$");
     public static final GameInitCommands loadGame = new GameInitCommands("^\\s*load\\s+game\\s*$");
-    public static final GameInitCommands gameMap = new GameInitCommands("^\\s*game\\s+map\\s+(?<mapNumber>\\d+)\\s*$");
+    public static final GameInitCommands gameMap = new GameInitCommands("^\\s*game\\s+map\\s+(\\d+)\\s*$");
 
     GameInitCommands(String regex) {
         super(regex);
