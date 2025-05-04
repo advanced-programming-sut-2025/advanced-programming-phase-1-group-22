843864c04101fc1a6561b03e12b90b047d675a69
A-A-MEHR18 <a.a.mehr.18@gmail.com>
2025-05-04 21:05:22 +0330

    debugging walk

--- src/main/java/model/WalkingStrategy.java
+++ src/main/java/model/WalkingStrategy.java
@@ -1,10 +1,31 @@
 package model;
 
+import model.animal.Animal;
+import model.animal.Fish;
+import model.craft.Craft;
+import model.products.AnimalProduct;
+import model.products.TreesAndFruitsAndSeeds.Tree;
+import model.relations.NPC;
+import model.relations.Player;
+import model.shelter.FarmBuilding;
+import model.shelter.ShippingBin;
+import model.source.Crop;
+import model.source.Mineral;
+import model.source.MixedSeeds;
+import model.source.Seed;
+import model.structure.NPCHouse;
+import model.structure.Stone;
+import model.structure.Structure;
+import model.structure.Trunk;
+import model.structure.farmInitialElements.Cottage;
+import model.structure.farmInitialElements.GreenHouse;
+import model.structure.farmInitialElements.Lake;
+import model.structure.farmInitialElements.Quarry;
+import model.structure.stores.Store;
+import model.tools.Tool;
 import utils.App;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class WalkingStrategy {
     private final Map<Pair, Integer> distances = new HashMap<>();
@@ -22,26 +43,68 @@ public class WalkingStrategy {
     }
 
     public boolean calculateTile(Pair origin, Pair dest, int distance) {
+        if (dest.getX() < 0 || dest.getX() >= 160 || dest.getY() < 0 || dest.getY() >= 120) return false;
+        if (!app.getCurrentGame().getTiles()[dest.getX()][dest.getY()].isPassable()) return false;
         if (Objects.equals(origin.getX(), dest.getX()) && Objects.equals(origin.getY(), dest.getY())) {
             this.distance = distance;
             return true;
         }
         int[] xs = {1,1,0,-1,-1,-1,0,1};
         int[] ys = {0,1,1,1,0,-1,-1,-1};
-        Pair newPair;
-        for (Pair pair : distances.keySet()) {
-            if (distances.get(pair) == distance) {
-                for (int i = 0; i < 8; i++) {
-                    newPair = new Pair(origin.getX() + xs[i], origin.getY() + ys[i]);
-                    if (!app.getCurrentGame().getTiles()[newPair.getX()][newPair.getY()].isPassable()) continue;
-                    if (distances.containsKey(newPair)) continue;
-                    distances.put(newPair, distance + 1);
-                    if (calculateTile(newPair, dest, distance + 1)) {
-                        return true;
+        Pair newPair = new Pair(origin.getX(), origin.getY());
+        boolean flag = true;
+        while (flag) {
+//            printMap();
+//            System.out.println("\n\n\n");
+            flag = false;
+            Set<Pair> set = new HashSet<>(distances.keySet());
+            for (Pair pair : set) {
+                if (distances.get(pair) == distance) {
+                    for (int i = 0; i < 8; i++) {
+                        newPair = new Pair(pair.getX() + xs[i], pair.getY() + ys[i]);
+
+                        if (newPair.getX() < 0 || newPair.getX() >= 160 || newPair.getY() < 0 || newPair.getY() >= 120)
+                            continue;
+                        if (!app.getCurrentGame().getTiles()[newPair.getX()][newPair.getY()].isPassable()) continue;
+                        boolean flag2 = false;
+                        for (Pair pair1 : distances.keySet()) {
+                            if (pair1.getX() == newPair.getX() && pair1.getY() == newPair.getY()) {
+                                flag2 = true;
+                                break;
+                            }
+                        }
+                        if (flag2) continue;
+                        distances.put(newPair, distance + 1);
+                        flag = true;
+                        if (Objects.equals(newPair.getX(), dest.getX()) && Objects.equals(newPair.getY(), dest.getY())) {
+                            this.distance = distance;
+                            return true;
+                        }
                     }
                 }
             }
+            distance++;
         }
         return false;
+    }
+
+    private void printMap() {
+        Character[][] str = new Character[160][120];
+        for (int i = 0; i < 120; i++) {
+            for (int j = 0; j < 160; j++) {
+                str[j][i] = ' ';
+            }
+        }
+        for (Pair pair : distances.keySet()) {
+            str[pair.getX()][pair.getY()] = 'A';
+        }
+
+
+        for (int i = 0; i < 120; i++) {
+            for (int j1 = 0; j1 < 160; j1++) {
+                System.out.print(str[j1][120 - 1 - i]);
+            }
+            System.out.println();
+        }
     }
 }
