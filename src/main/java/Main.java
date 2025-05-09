9e61ade9549f0d510e4bd6eea08499ecf87721b0
MahdiArabi2006 <mahdi.ar1384.ir@gmail.com>
2025-05-09 20:02:49 +0330

    merge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import jakarta.persistence.EntityManagerFactory;
-import save.GameState;
-import utils.App;
 import utils.HibernateUtil;
 import view.ViewRender;
 
@@ -8,8 +6,6 @@ public class Main {
     public static void main(String[] args) {
         EntityManagerFactory entityManagerFactory = HibernateUtil.getEntityManagerFactory();
         entityManagerFactory.createEntityManager();
-        String json = new GameState(App.getInstance().getCurrentGame()).toJson();
-        GameState state = GameState.fromJson(json);
         new ViewRender().run();
     }
 }
