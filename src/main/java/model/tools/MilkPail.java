fd20d336c120344e8e87ada3a0dfcb4e62bc4b8f
MahdiArabi2006 <mahdi.ar1384.ir@gmail.com>
2025-05-11 02:06:40 +0330

    add cost

--- src/main/java/model/tools/MilkPail.java
+++ src/main/java/model/tools/MilkPail.java
@@ -14,87 +14,89 @@ import java.util.List;
 
 @Getter
 public class MilkPail implements Tool {
-    private static MilkPail instance;
+	private static MilkPail instance;
 
-    private MilkPail() {
-    }
+	private MilkPail() {
+	}
 
-    public static MilkPail getInstance() {
-        if (instance == null) {
-            instance = new MilkPail();
-        }
-        return instance;
-    }
+	public static MilkPail getInstance() {
+		if (instance==null) {
+			instance = new MilkPail();
+		}
+		return instance;
+	}
 
-    private final Integer energyCost = 4;
-    private final Integer price = 100;
+	private final Integer energyCost = 4;
+	private final Integer price = 1000 / 2;
 
-    @Override
-    public void addToolEfficiency(double efficiency) {
+	@Override
+	public void addToolEfficiency(double efficiency) {
 
-    }
+	}
 
-    @Override
-    public String getName() {
-        return "milkpail";
-    }
+	@Override
+	public String getName() {
+		return "milkpail";
+	}
 
-    @Override
-    public int getSellPrice() {
-        return 0;
-    }
+	@Override
+	public int getSellPrice() {
+		return this.price;
+	}
 
-    @Override
-    public Tool getToolByLevel(int level) {
-        return null;
-    }
+	@Override
+	public Tool getToolByLevel(int level) {
+		return null;
+	}
 
-    @Override
-    public int getLevel() {
-        return 0;
-    }
+	@Override
+	public int getLevel() {
+		return 0;
+	}
 
-    @Override
-    public int getEnergy(Player player) {
-        int minus = 0;
-        if (player.getAbilityLevel(Ability.FARMING) == 4){
-            minus += 1;
-        }
-        if (player.getBuffAbility() != null && player.getBuffAbility().equals(Ability.FARMING)){
-            minus += 1;
-        }
-        return (int) (App.getInstance().getCurrentGame().getWeatherCoefficient() * energyCost - minus);
-    }
+	@Override
+	public int getEnergy(Player player) {
+		int minus = 0;
+		if (player.getAbilityLevel(Ability.FARMING)==4) {
+			minus += 1;
+		}
+		if (player.getBuffAbility()!=null && player.getBuffAbility().equals(Ability.FARMING)) {
+			minus += 1;
+		}
+		return (int) (App.getInstance().getCurrentGame().getWeatherCoefficient() * energyCost - minus);
+	}
 
-    @Override
-    public String useTool(Player player, Tile tile) {
-        List<Structure> structures = App.getInstance().getCurrentGame().getVillage().findStructuresByTile(tile);
-        for (Structure structure : structures) {
-            if (structure != null){
-                if (structure instanceof Animal currentAnimal){
-                    if (currentAnimal.getAnimalType().equals(AnimalType.COW) ||
-                            currentAnimal.getAnimalType().equals(AnimalType.GOAT)){
-                        player.changeEnergy(-this.getEnergy(player));
-                        if (currentAnimal.getTodayProduct() == null){
-                            return "this animal do not have product";
-                        }
-                        if (player.getInventory().isInventoryHaveCapacity(currentAnimal.getTodayProduct())){
-                            AnimalProduct animalProduct = currentAnimal.getTodayProduct();
-                            player.getInventory().addProductToBackPack(animalProduct,1);
-                            currentAnimal.setTodayProduct(null);
-                            currentAnimal.changeFriendShip(5);
-                            return "you collect produce of " + currentAnimal.getName() + ": " + animalProduct.getName() +
-                                    " with quality: " + animalProduct.getProductQuality();
-                        }
-                        return "your inventory is full so you can not milk animal";
-                    }
-                }
-            }
-        }
-        player.changeEnergy(-this.getEnergy(player));
-        return "you use this tool in a wrong way";
-    }
+	@Override
+	public String useTool(Player player, Tile tile) {
+		List<Structure> structures = App.getInstance().getCurrentGame().getVillage().findStructuresByTile(tile);
+		for (Structure structure : structures) {
+			if (structure!=null) {
+				if (structure instanceof Animal currentAnimal) {
+					if (currentAnimal.getAnimalType().equals(AnimalType.COW) ||
+							currentAnimal.getAnimalType().equals(AnimalType.GOAT)) {
+						player.changeEnergy(-this.getEnergy(player));
+						if (currentAnimal.getTodayProduct()==null) {
+							return "this animal do not have product";
+						}
+						if (player.getInventory().isInventoryHaveCapacity(currentAnimal.getTodayProduct())) {
+							AnimalProduct animalProduct = currentAnimal.getTodayProduct();
+							player.getInventory().addProductToBackPack(animalProduct, 1);
+							currentAnimal.setTodayProduct(null);
+							currentAnimal.changeFriendShip(5);
+							return "you collect produce of " + currentAnimal.getName() + ": " + animalProduct.getName() +
+									" with quality: " + animalProduct.getProductQuality();
+						}
+						return "your inventory is full so you can not milk animal";
+					}
+				}
+			}
+		}
+		player.changeEnergy(-this.getEnergy(player));
+		return "you use this tool in a wrong way";
+	}
 
-    @Override
-    public Integer getContainingEnergy() {return 0;}
+	@Override
+	public Integer getContainingEnergy() {
+		return 0;
+	}
 }
