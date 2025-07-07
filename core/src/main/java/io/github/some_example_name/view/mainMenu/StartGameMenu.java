53aaad3e0a1247c603d32de846843a9dcc50857c
Roham8406 <Roham8406@gmail.com>
2025-07-07 18:48:16 +0330

    implemented terminate menu and refactor start menu

--- core/src/main/java/io/github/some_example_name/view/mainMenu/StartGameMenu.java
+++ core/src/main/java/io/github/some_example_name/view/mainMenu/StartGameMenu.java
@@ -1,14 +1,7 @@
 package io.github.some_example_name.view.mainMenu;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.Timer;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
-import io.github.some_example_name.MainGradle;
 import io.github.some_example_name.controller.mainMenu.StartGameMenuController;
 import io.github.some_example_name.model.FarmType;
 import io.github.some_example_name.model.PlayerType;
@@ -16,20 +9,18 @@ import io.github.some_example_name.variables.Session;
 import lombok.Getter;
 
 @Getter
-public class StartGameMenu implements Screen {
-    private Stage stage;
+public class StartGameMenu extends PreGameMenu {
     private final TextButton newGameButton;
     private final TextButton loadGameButton;
     private final TextButton enterGameButton;
     private final SelectBox<String> farmSelection;
     private final SelectBox<String> playerSelection;
     private final Integer state;
-    private final Table table;
-    private final Skin skin;
     private final StartGameMenuController controller;
 
-    public StartGameMenu(StartGameMenuController controller, Skin skin, Integer state) {
-        this.controller = controller;
+    public StartGameMenu(Skin skin, Integer state) {
+        super(skin);
+        this.controller = StartGameMenuController.getInstance();
         this.newGameButton = new TextButton("New Game!", skin);
         this.loadGameButton = new TextButton("Load Game!", skin);
         this.enterGameButton = new TextButton("Enter!", skin);
@@ -46,19 +37,12 @@ public class StartGameMenu implements Screen {
                 playerNames.add(value.getName());
         }
         playerSelection.setItems(playerNames);
-        this.table = new Table();
-        this.skin = skin;
         this.state = state;
         controller.setView(this);
     }
 
     @Override
-    public void show() {
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
-
-        table.setFillParent(true);
-        table.center();
+    protected void showStage() {
         switch (state) {
             case 0: {
                 table.add(newGameButton).width(400).row();
@@ -74,67 +58,16 @@ public class StartGameMenu implements Screen {
             }
             break;
         }
-        stage.addActor(table);
     }
 
     @Override
-    public void render(float delta) {
-        MainGradle.getInstance().getBatch().begin();
-        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+    public void renderStage(float delta) {
         stage.draw();
-        MainGradle.getInstance().getBatch().end();
         controller.handleMainMenuButtons();
     }
 
-    @Override
-    public void resize(int width, int height) {
-
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-
-    }
-
-
-    public void alert(String message, Integer timer) {
-        final Table alertBox = new Table(skin);
-        alertBox.setSize(1000, 50);
-        alertBox.setPosition(Gdx.graphics.getWidth() / 2f, 0, Align.bottom);
-
-        Label label = new Label(message, skin);
-        alertBox.add(label);
-        alertBox.setBackground("progress-bar-health-knob");
-        stage.addActor(alertBox);
-
-        Timer.schedule(new Timer.Task() {
-            @Override
-            public void run() {
-                alertBox.remove();
-            }
-        }, timer);
-
-    }
-
     public void setState(int i) {
-        this.dispose();
-        MainGradle.getInstance().setScreen(new StartGameMenu(controller, skin, i));
+        setScreen(new StartGameMenu(skin, i));
     }
-
 }
 
