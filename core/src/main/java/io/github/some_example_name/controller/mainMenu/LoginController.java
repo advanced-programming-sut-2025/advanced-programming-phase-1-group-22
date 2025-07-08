7d8a45c8f5d06b86f3658604db611bf11e17aaf8
MahdiArabi <mahdi.ar1384.ir@gmail.com>
2025-07-08 20:13:01 +0330

    add forget password to login menu

--- core/src/main/java/io/github/some_example_name/controller/mainMenu/LoginController.java
+++ core/src/main/java/io/github/some_example_name/controller/mainMenu/LoginController.java
@@ -3,6 +3,7 @@ package io.github.some_example_name.controller.mainMenu;
 import io.github.some_example_name.model.records.Response;
 import io.github.some_example_name.repository.UserRepo;
 import io.github.some_example_name.service.AccountService;
+import io.github.some_example_name.utils.GeneratePassword;
 import io.github.some_example_name.utils.PasswordHasher;
 import io.github.some_example_name.view.ViewRender;
 import io.github.some_example_name.view.mainMenu.Login;
@@ -27,6 +28,24 @@ public class LoginController {
         if (response.shouldBeBack()) return true;
         view.alert(response.message(), 5);
         return false;
+    }
+
+    public boolean forgetPassword() {
+        if (notFilled(view.getAnswer().getText(), view.getUsernameForDialog().getText(), view.getNewPassword().getText())) {
+            view.alert("You should fill all fields", 5);
+            return false;
+        }
+        Response response = service.forgetPassword(view.getUsernameForDialog().getText(), view.getAnswer().getText(), view.getNewPassword().getText());
+        if (response.shouldBeBack()){
+            view.alert(response.message(),5);
+            return true;
+        }
+        view.alert(response.message(), 5);
+        return false;
+    }
+
+    public String getRandomPassword() {
+        return GeneratePassword.generatePassword();
     }
 
     private boolean notFilled(String... strings) {
