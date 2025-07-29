9dfd00191e7050c6d4b86275740672a2f9743740
MahdiArabi <mahdi.ar1384.ir@gmail.com>
2025-07-29 16:30:18 +0330

    add real-time map menu

--- core/src/main/java/io/github/some_example_name/client/view/MiniMapActor.java
+++ core/src/main/java/io/github/some_example_name/client/view/MiniMapActor.java
@@ -7,17 +7,30 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import io.github.some_example_name.common.model.Farm;
 import io.github.some_example_name.common.model.Tile;
 import io.github.some_example_name.common.model.TileType;
+import io.github.some_example_name.common.model.animal.Animal;
+import io.github.some_example_name.common.model.dto.SpriteComponent;
+import io.github.some_example_name.common.model.dto.SpriteHolder;
 import io.github.some_example_name.common.model.structure.Structure;
 import io.github.some_example_name.common.model.structure.farmInitialElements.Lake;
 import io.github.some_example_name.common.utils.App;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MiniMapActor extends Actor {
     private final float scale;
+    private float currentDelta = 0f;
 
     public MiniMapActor(float scale) {
         this.scale = scale;
         setSize(App.getInstance().getCurrentGame().tiles.length * App.tileWidth * scale,
             App.getInstance().getCurrentGame().tiles[0].length * App.tileHeight * scale);
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+        this.currentDelta = delta;
     }
 
     @Override
@@ -27,7 +40,7 @@ public class MiniMapActor extends Actor {
 
         batch.begin();
         drawTiles(batch);
-        drawStructures(batch);
+        drawStructures(batch, currentDelta);
     }
 
     private void drawTiles(Batch batch) {
@@ -51,10 +64,11 @@ public class MiniMapActor extends Actor {
         }
     }
 
-    private void drawStructures(Batch batch) {
+    private void drawStructures(Batch batch, float delta) {
         for (Farm farm : App.getInstance().getCurrentGame().getVillage().getFarms()) {
             farm.applyPendingChanges();
-            for (Structure structure : farm.getStructuresSnapshot()) {
+            List<Structure> structures = farm.getStructuresSnapshot();
+            for (Structure structure : structures.toArray(new Structure[0])) {
                 if (structure.getSprite() != null) {
                     if (structure instanceof Lake) {
                         for (Tile tile : structure.getTiles()) {
@@ -64,25 +78,74 @@ public class MiniMapActor extends Actor {
                                 getY() + tile.getY() * App.tileHeight * scale);
                             sprite.draw(batch);
                         }
+                    } else if (structure instanceof Animal animal) {
+                        if (animal.getIsAnimalStayOutAllNight()) {
+                            Sprite sprite = new Sprite(structure.getSprite());
+                            drawRawSprite(sprite, structure, batch);
+                        }
                     } else {
                         Sprite sprite = new Sprite(structure.getSprite());
-                        sprite.setSize(structure.getSprite().getWidth() * scale, structure.getSprite().getHeight() * scale);
-                        sprite.setPosition(getX() + structure.getTiles().get(0).getX() * App.tileWidth * scale,
-                            getY() + structure.getTiles().get(0).getY() * App.tileHeight * scale);
-                        sprite.draw(batch);
+                        drawRawSprite(sprite, structure, batch);
                     }
+                }
+                if (structure.getSprites() != null) {
+                    for (SpriteHolder sprite : structure.getSprites()) {
+                        drawSpriteHolder(sprite, structure, batch);
+                    }
+                }
+                if (structure.getSpriteComponent() != null) {
+                    drawSpriteComponent(delta, structure.getSpriteComponent(), structure, batch);
                 }
             }
         }
         App.getInstance().getCurrentGame().getVillage().applyPendingChanges();
-        App.getInstance().getCurrentGame().getVillage().forEachStructure(structure -> {
+        for (Structure structure : App.getInstance().getCurrentGame().getVillage().getStructuresSnapshot()) {
             if (structure.getSprite() != null) {
                 Sprite sprite = new Sprite(structure.getSprite());
-                sprite.setSize(structure.getSprite().getWidth() * scale, structure.getSprite().getHeight() * scale);
-                sprite.setPosition(getX() + structure.getTiles().get(0).getX() * App.tileWidth * scale,
-                    getY() + structure.getTiles().get(0).getY() * App.tileHeight * scale);
-                sprite.draw(batch);
+                drawRawSprite(sprite, structure, batch);
             }
-        });
+            if (structure.getSprites() != null) {
+                for (SpriteHolder sprite : structure.getSprites()) {
+                    drawSpriteHolder(sprite, structure, batch);
+                }
+            }
+            if (structure.getSpriteComponent() != null) {
+                drawSpriteComponent(delta, structure.getSpriteComponent(), structure, batch);
+            }
+        }
+    }
+
+    private void drawRawSprite(Sprite sprite, Structure structure, Batch batch) {
+        sprite.setSize(structure.getSprite().getWidth() * scale, structure.getSprite().getHeight() * scale);
+        sprite.setPosition(getX() + structure.getTiles().get(0).getX() * App.tileWidth * scale,
+            getY() + structure.getTiles().get(0).getY() * App.tileHeight * scale);
+        int width = structure.getWidth() == null ? 1 : structure.getWidth();
+        int height = structure.getHeight() == null ? 1 : structure.getHeight();
+        sprite.setPosition(getX() + (width / 2f + structure.getTiles().get(0).getX()) * App.tileWidth * scale - sprite.getWidth() / 2,
+            getY() + (height / 2f + structure.getTiles().get(0).getY()) * App.tileHeight * scale - sprite.getHeight() / 2);
+        sprite.draw(batch);
+    }
+
+    private void drawSpriteHolder(SpriteHolder sprite, Structure structure, Batch batch) {
+        if (structure.getTiles().isEmpty()) return;
+        Sprite copy = new Sprite(sprite.getSprite());
+        copy.setSize(copy.getWidth() * scale, copy.getHeight() * scale);
+        copy.setPosition(
+            getX() + (sprite.getOffset().getX() + structure.getTiles().get(0).getX()) * App.tileWidth * scale,
+            getY() + (sprite.getOffset().getY() + structure.getTiles().get(0).getY()) * App.tileHeight * scale);
+        copy.draw(batch);
+    }
+
+    private void drawSpriteComponent(float delta, SpriteComponent sprite, Structure structure, Batch batch) {
+        ArrayList<SpriteHolder> spriteHolders = structure.getSpriteComponent().getSprites(delta);
+        for (SpriteHolder spriteHolder : spriteHolders) {
+            Sprite copy = new Sprite(spriteHolder.getSprite());
+            copy.setSize(copy.getWidth() * scale, copy.getHeight() * scale);
+            copy.setPosition(
+                getX() + (sprite.getOffset().getX() + spriteHolder.getOffset().getX() + structure.getTiles().get(0).getX()) * App.tileWidth * scale,
+                getY() + (sprite.getOffset().getY() + spriteHolder.getOffset().getY() + structure.getTiles().get(0).getY()) * App.tileHeight * scale
+            );
+            copy.draw(batch);
+        }
     }
 }
