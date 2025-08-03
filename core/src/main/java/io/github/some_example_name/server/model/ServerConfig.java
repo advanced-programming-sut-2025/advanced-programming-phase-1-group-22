bb388c9bc8eb331b396197b0e98f32a7ab374fd9
Roham8406 <Roham8406@gmail.com>
2025-08-03 14:21:47 +0330

    untracked serviceConfig

--- core/src/main/java/io/github/some_example_name/server/model/ServerConfig.java
+++
@@ -1,6 +0,0 @@
-package io.github.some_example_name.server.model;
-
-public class ServerConfig {
-    public static final String API_URL = "https://openrouter.ai/api/v1/chat/completions";
-    public static final String API_KEY = "sk-or-v1-HAHAHA";
-}
