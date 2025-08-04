5910b90b9dd3f568a11b9a6ba6493c4bda6dce5d
MahdiArabi <mahdi.ar1384.ir@gmail.com>
2025-08-04 20:26:56 +0330

    add GreenHouseRecipe

---
+++ core/src/main/java/io/github/some_example_name/client/view/mainMenu/GreenHouseRecipe.java
@@ -0,0 +1,66 @@
+package io.github.some_example_name.client.view.mainMenu;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import io.github.some_example_name.client.MainGradle;
+import io.github.some_example_name.client.controller.WorldController;
+import io.github.some_example_name.common.model.Salable;
+
+import java.util.ArrayList;
+
+public class GreenHouseRecipe extends PopUp {
+    @Override
+    public void createMenu(Stage stage, Skin skin, WorldController playerController) {
+        super.createMenu(stage, skin, playerController);
+        Window window = new Window("", skin);
+        window.setSize(600, 250);
+        window.setPosition(
+            (MainGradle.getInstance().getCamera().viewportWidth - window.getWidth()) / 2f,
+            (MainGradle.getInstance().getCamera().viewportHeight - window.getHeight()) / 2f
+        );
+        window.setMovable(false);
+
+        Table info = new Table();
+        info.top().left().pad(10).defaults().pad(5).left();
+        info.add(new Label("need to build: ", skin)).row();
+        info.add(new Label("    - 1000 Gold", skin)).row();
+        info.add(new Label("    - 500 wood", skin)).row();
+        info.add(new Label("click on greenhouse to build", skin)).row();
+
+        ArrayList<Actor> array = new ArrayList<>();
+        array.add(window);
+        ImageButton exitButton = provideExitButton(array);
+
+        Table content = new Table();
+        content.setFillParent(true);
+        content.add(info).padTop(20).expand().top();
+        window.add(content).expand().fill().pad(10);
+        Group group = new Group() {
+            @Override
+            public void act(float delta) {
+                window.setPosition(
+                    (MainGradle.getInstance().getCamera().viewportWidth - window.getWidth()) / 2f +
+                        MainGradle.getInstance().getCamera().position.x - MainGradle.getInstance().getCamera().viewportWidth / 2,
+                    (MainGradle.getInstance().getCamera().viewportHeight - window.getHeight()) / 2f +
+                        MainGradle.getInstance().getCamera().position.y - MainGradle.getInstance().getCamera().viewportHeight / 2
+                );
+                exitButton.setPosition(
+                    window.getX() + window.getWidth() - exitButton.getWidth() / 2f + 16,
+                    window.getY() + window.getHeight() - exitButton.getHeight() / 2f
+                );
+
+                super.act(delta);
+            }
+        };
+        group.addActor(window);
+        group.addActor(exitButton);
+        getMenuGroup().addActor(group);
+    }
+
+    @Override
+    protected void handleDragRelease(InputEvent event, float x, float y, int pointer, Image itemImage, Salable item, Image dragImage, Boolean flag) {
+    }
+}
